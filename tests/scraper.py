e41022fc8d1e8e44562dd5181102b0500b322185
Karol Lukaszczyk <lukkarcontact@gmail.com>
2023-11-30 19:21:44 +0100

    refactored

--- tests/scraper.py
+++ tests/scraper.py
@@ -3,10 +3,9 @@ import csv
 import os
 import shutil
 import unittest
+import datetime
 
 # Third-party imports
-import datetime
-from selenium.common.exceptions import WebDriverException
 import enlighten
 
 # Local imports
@@ -14,8 +13,8 @@ from scraper._utils.selenium_utils import humans_delay
 from scraper._utils.string_transformations import transform_location_to_url_format
 from scraper.config import DATA, SCRAPER, DOMAINS
 from scraper.scrape.olx.process_olx_site_offers import process_domain_offers_olx
-from scraper.scrape.process_sites_offers import scrape_offers
 from scraper.webdriver_setup import get_driver
+from scraper.scrape.process_sites_offers import scrape_offers
 
 
 class TestScraper(unittest.TestCase):
@@ -40,14 +39,9 @@ class TestScraper(unittest.TestCase):
                 folder_path = os.path.join(self.scraped_folder, folder)
                 shutil.rmtree(folder_path)
 
-        self.new_folders = None
-
-        humans_delay()
+        self.driver.quit()
 
     def test_end_to_end(self):
-        """
-        End-to-End Test: Run the entire script from start to finish.
-        """
         try:
             search_criteria = self.search_criteria.copy()
             search_criteria["location_query"] = self.location_query["low_volume"]
@@ -55,139 +49,49 @@ class TestScraper(unittest.TestCase):
             scrape_offers(self.driver, search_criteria)
         except Exception as e:
             self.fail(f"Scrape offers failed with {e}")
-        finally:
-            self.driver.quit()
 
-        existing_folders_after = set(os.listdir(self.scraped_folder))
-        self.new_folders = existing_folders_after - self.existing_folders_before
-
-        self.assertNotEqual(
-            self.existing_folders_before,
-            existing_folders_after,
-            "No new data files were created.",
-        )
-        self.assertEqual(
-            len(self.new_folders),
-            1,
-            "There should be one folder for all created data.",
-        )
-
-        for folder in self.new_folders:
-            files = os.listdir(os.path.join(self.scraped_folder, folder))
-            self.assertGreaterEqual(
-                len(files),
-                1,
-                f"There should be at least one file in {folder}.",
-            )
-            for file in files:
-                self.assertTrue(
-                    file.endswith(".csv"), f"File {file} should be a csv file."
-                )
-
-            number_of_data_rows = 0
-            for file in files:
-                file_path = os.path.join(self.scraped_folder, folder, file)
-                with open(
-                    file_path, "r", newline="", encoding=DATA["encoding"]
-                ) as csvfile:
-                    reader = csv.reader(csvfile)
-                    rows = list(reader)
-                    # Assuming the first row is the header
-                    number_of_data_rows = len(rows) - 1
-            self.assertTrue(
-                search_criteria["scraped_offers_cap"] == number_of_data_rows,
-                f"The CSV file {file} should contain rows at the same length as the cap.",
-            )
+        self._verify_scraping_results()
 
     def test_olx_scrape_offers(self):
-        """
-        Test: Scrape offers from OLX.
-        """
         try:
-            search_criteria = self.search_criteria.copy()
-            search_criteria["location_query"] = self.location_query["high_volume"]
-            offers_cap = search_criteria["scraped_offers_cap"]
-
-            progress = enlighten.Counter(
-                desc="Total progress",
-                unit="offers",
-                color="green",
-                total=offers_cap,
-            )
-
-            formatted_location = transform_location_to_url_format(
-                search_criteria["location_query"]
-            )
-            url = f'{DOMAINS["olx"]["domain"]}/{DOMAINS["olx"]["category"]}q-{formatted_location}/'
-            timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-            scraped_urls: set[str] = set()
-
-            humans_delay()
-            self.driver.get(url)
-
-            process_domain_offers_olx(
-                self.driver, search_criteria, timestamp, progress, scraped_urls
-            )
+            self._setup_and_scrape_offers("high_volume")
         except Exception as e:
             self.fail(f"Scrape offers failed with {e}")
-        finally:
-            self.driver.quit()
 
-        existing_folders_after = set(os.listdir(self.scraped_folder))
-        self.new_folders = existing_folders_after - self.existing_folders_before
-
-        self.assertNotEqual(
-            self.existing_folders_before,
-            existing_folders_after,
-            "No new data files were created.",
-        )
-        self.assertEqual(
-            len(self.new_folders),
-            1,
-            "There should be one folder for all created data.",
-        )
-
-        for folder in self.new_folders:
-            files = os.listdir(os.path.join(self.scraped_folder, folder))
-            self.assertGreaterEqual(
-                len(files),
-                1,
-                f"There should be at least one file in {folder}.",
-            )
-            for file in files:
-                self.assertTrue(
-                    file.endswith(".csv"), f"File {file} should be a csv file."
-                )
-
-            number_of_data_rows = 0
-            for file in files:
-                file_path = os.path.join(self.scraped_folder, folder, file)
-                with open(
-                    file_path, "r", newline="", encoding=DATA["encoding"]
-                ) as csvfile:
-                    reader = csv.reader(csvfile)
-                    rows = list(reader)
-                    # Assuming the first row is the header
-                    number_of_data_rows += len(rows) - 1
-            self.assertTrue(
-                search_criteria["scraped_offers_cap"] == number_of_data_rows,
-                f"The CSV file {file} should contain rows at the same length as the cap.",
-            )
+        self._verify_scraping_results()
 
     def test_otodom_scrape_offers(self):
-        """
-        Test: Scrape offers from otodom.com.
-        """
         try:
-            search_criteria = self.search_criteria.copy()
-            search_criteria["location_query"] = self.location_query["high_volume"]
-
-            scrape_offers(self.driver, search_criteria)
+            self._setup_and_scrape_offers("high_volume")
         except Exception as e:
             self.fail(f"Scrape offers failed with {e}")
-        finally:
-            self.driver.quit()
 
+        self._verify_scraping_results()
+
+    def _setup_and_scrape_offers(self, volume_type):
+        search_criteria = self.search_criteria.copy()
+        search_criteria["location_query"] = self.location_query[volume_type]
+        offers_cap = search_criteria["scraped_offers_cap"]
+
+        progress = enlighten.Counter(
+            desc="Total progress", unit="offers", color="green", total=offers_cap
+        )
+
+        formatted_location = transform_location_to_url_format(
+            search_criteria["location_query"]
+        )
+        url = f'{DOMAINS["olx"]["domain"]}/{DOMAINS["olx"]["category"]}q-{formatted_location}/'
+        timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        scraped_urls: set[str] = set()
+
+        humans_delay()
+        self.driver.get(url)
+
+        process_domain_offers_olx(
+            self.driver, search_criteria, timestamp, progress, scraped_urls
+        )
+
+    def _verify_scraping_results(self):
         existing_folders_after = set(os.listdir(self.scraped_folder))
         self.new_folders = existing_folders_after - self.existing_folders_before
 
@@ -197,38 +101,44 @@ class TestScraper(unittest.TestCase):
             "No new data files were created.",
         )
         self.assertEqual(
-            len(self.new_folders),
-            1,
-            "There should be one folder for all created data.",
+            len(self.new_folders), 1, "There should be one folder for all created data."
         )
 
-        for folder in self.new_folders:
-            files = os.listdir(os.path.join(self.scraped_folder, folder))
+        self.check_csv_files()
+
+    def check_csv_files(self):
+        for new_folder in self.new_folders:
+            new_files = os.listdir(os.path.join(self.scraped_folder, new_folder))
             self.assertGreaterEqual(
-                len(files),
-                1,
-                f"There should be at least one file in {folder}.",
+                len(new_files), 1, f"Expected at least one file in {new_folder}."
             )
-            for file in files:
+            for new_file in new_files:
                 self.assertTrue(
-                    file.endswith(".csv"), f"File {file} should be a csv file."
+                    new_file.endswith(".csv"), f"Expected a CSV file, got {new_file}."
                 )
 
-            number_of_data_rows = 0
-            for file in files:
-                file_path = os.path.join(self.scraped_folder, folder, file)
+        counted_rows = 0
+        for new_file in os.listdir(
+            os.path.join(self.scraped_folder, next(iter(self.new_folders)))
+        ):
+            if new_file.endswith(".csv"):
                 with open(
-                    file_path, "r", newline="", encoding=DATA["encoding"]
+                    os.path.join(
+                        self.scraped_folder, next(iter(self.new_folders)), new_file
+                    ),
+                    "r",
+                    newline="",
+                    encoding=DATA["encoding"],
                 ) as csvfile:
                     reader = csv.reader(csvfile)
-                    rows = list(reader)
-                    # Assuming the first row is the header
-                    number_of_data_rows += len(rows) - 1
+                    counted_rows += len(list(reader)) - 1
 
-            self.assertTrue(
-                search_criteria["scraped_offers_cap"] == number_of_data_rows,
-                f"The CSV file {file} should contain rows at the same length as the cap.",
-            )
+        expected_rows = self.search_criteria["scraped_offers_cap"]
+        self.assertEqual(
+            counted_rows,
+            expected_rows,
+            f"Expected {expected_rows} data rows in {new_file}, found {counted_rows}.",
+        )
 
 
 if __name__ == "__main__":
