ba190b6c464ff44009ba0e9816713f726c48304d
Karol Lukaszczyk <lukkarcontact@gmail.com>
2024-01-07 16:31:58 +0100

    update

--- dashboard/model_offer_predictor.py
+++ dashboard/model_offer_predictor.py
@@ -1,7 +1,25 @@
+"""
+This module provides a class, ModelPredictor, for loading a machine learning model and its
+metadata, preparing dataframes for prediction, and performing predictions using the model.
+
+Example Usage:
+--------------
+
+model_path = "notebooks\\gbm_model_file.p"
+metadata_path = "notebooks\\gbm_model_metadata.json"
+your_offers_path = os.getenv("YOUR_OFFERS_PATH", "data\\test\\your_offers.csv")
+
+predictor = ModelPredictor(model_path, metadata_path)
+predictor.get_price_predictions(your_offers_path)
+
+"""
+
+# Standard imports
+import json
+
+# Third-party imports
 import pandas as pd
 import pickle
-import json
-import os
 
 
 class ModelPredictor:
@@ -24,12 +42,12 @@ class ModelPredictor:
         get_price_predictions(your_offers_path): Loads offers data, predicts prices, and displays results.
     """
 
-    def __init__(self, model_path, metadata_path):
+    def __init__(self, model_path: str, metadata_path: str):
         self.model, self.metadata = self._load_model_and_metadata(
             model_path, metadata_path
         )
 
-    def get_price_predictions(self, your_offers_path):
+    def get_price_predictions(self, offers_path: str):
         """
         Loads the machine learning model and its associated metadata from the specified file paths.
 
@@ -42,7 +60,7 @@ class ModelPredictor:
         """
 
         if self.model is not None and self.metadata is not None:
-            your_offers_df = pd.read_csv(your_offers_path)  # Load your offers dataframe
+            your_offers_df = pd.read_csv(offers_path)  # Load your offers dataframe
             result_df = self._predict_and_merge(your_offers_df)
 
             if result_df is not None:
@@ -53,23 +71,30 @@ class ModelPredictor:
         else:
             print("Model or metadata loading failed.")
 
-    def _load_model_and_metadata(self, model_path, metadata_path):
+    def _load_model_and_metadata(self, model_path: str, metadata_path: str):
         try:
             # Load the trained model
             with open(model_path, "rb") as pickled:
                 data = pickle.load(pickled)
                 model = data["model"]
 
-            # Load the metadata (column names and dtypes)
+            # Load the metadata with size limitation for security
+            max_metadata_size = 10 * 1024 * 1024  # 10 MB size limit
             with open(metadata_path, "r") as file:
-                metadata = json.load(file)
+                file_content = file.read(
+                    max_metadata_size
+                )  # Read only the first 10 MB of the file
+                if file.tell() >= max_metadata_size:
+                    print("Metadata file size exceeds the limit of 10 MB.")
+                    return None, None
+                metadata = json.loads(file_content)
 
             return model, metadata
         except Exception as e:
             print(f"Error loading model or metadata: {e}")
             return None, None
 
-    def _prepare_dataframe(self, df):
+    def _prepare_dataframe(self, df: pd.DataFrame):
         try:
             # Create a copy of the dataframe to avoid modifying the original
             temp_df = df.copy()
@@ -92,7 +117,7 @@ class ModelPredictor:
             print(f"Error in preparing dataframe: {e}")
             return None
 
-    def _predict_and_merge(self, df):
+    def _predict_and_merge(self, df: pd.DataFrame):
         try:
             # Prepare the data frame
             prepared_df = self._prepare_dataframe(df)
@@ -108,12 +133,3 @@ class ModelPredictor:
         except Exception as e:
             print(f"Error in model prediction: {e}")
             return None
-
-
-# Example Usage
-model_path = "notebooks\\gbm_model_file.p"
-metadata_path = "notebooks\\gbm_model_metadata.json"
-your_offers_path = os.getenv("YOUR_OFFERS_PATH", "data\\test\\your_offers.csv")
-
-predictor = ModelPredictor(model_path, metadata_path)
-predictor.get_price_predictions(your_offers_path)
