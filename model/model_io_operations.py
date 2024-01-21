c6e24a7dc98a56753478a50f6a66b97c1196e3dd
Karol Lukaszczyk <lukkarcontact@gmail.com>
2024-01-21 14:11:56 +0100

    extracted model logic to separate module

--- model/model_io_operations.py
+++ model/model_io_operations.py
@@ -36,12 +36,67 @@ class ModelManager:
         check_if_loaded_properly: Check if the loaded model can make predictions.
     """
 
-    def __init__(self, model_path: str):
-        self.model_path = model_path
-        self.model_metadata_file_path = self._create_metadata_path(model_path)
+    def __init__(
+        self,
+        model_path_IO: str,
+        model: BaseEstimator,
+        training_data: pd.DataFrame = None,
+    ):
+        # Check if the directory exists
+        model_dir, model_dir_exists = self._model_dir_exists(model_path_IO)
+        if not model_dir_exists:
+            raise FileNotFoundError(f"The directory {model_dir} does not exist.")
 
-    def save_dataframe_metadata(self, df: pd.DataFrame):
+        self.model_path_IO = model_path_IO
+        self.model = model
+        self.model_metadata_file_path = self._create_metadata_path(model_path_IO)
+        self.training_data = training_data
+
+    def save_all_files_required(self, df: pd.DataFrame = None):
+        if df is None:
+            df = self.training_data
+            if df is None:
+                raise ValueError(
+                    "No DataFrame provided and no training data available."
+                )
+
+        self.save_dataframe_metadata(df)
+        self.save_model(self.model)
+
+    def load_model_and_metadata(self) -> (BaseEstimator, dict):
+        model = self.load_model()
+        metadata = self.load_metadata()
+        return model, metadata
+
+    def save_model(self, model: BaseEstimator = None, model_path_IO: str = None):
+        if model is None:
+            model = self.model
+            if model is None:
+                raise ValueError("No model provided and no model available.")
+        if model_path_IO is None:
+            model_path_IO = self.model_path_IO
+            if model_path_IO is None:
+                raise ValueError("No model path provided and no model path available.")
         try:
+            pickl = {"model": model}
+            with open(model_path_IO, "wb") as file:
+                pickle.dump(pickl, file)
+        except IOError as error:
+            raise IOError(f"Error saving model: {error}") from error
+        except Exception as error:
+            raise RuntimeError(
+                f"Unexpected error when saving model: {error}"
+            ) from error
+
+    def save_dataframe_metadata(self, df: pd.DataFrame = None):
+        try:
+            if df is None:
+                df = self.training_data
+                if df is None:
+                    raise ValueError(
+                        "No DataFrame provided and no training data available."
+                    )
+
             metadata = {
                 "columns": {col: str(df[col].dtype) for col in df.columns},
                 "column_order": list(df.columns),
@@ -54,6 +109,22 @@ class ModelManager:
         except Exception as error:
             raise RuntimeError(
                 f"Unexpected error when saving DataFrame metadata: {error}"
+            ) from error
+
+    def load_model(self) -> BaseEstimator:
+        try:
+            with open(self.model_path_IO, "rb") as pickled:
+                data = pickle.load(pickled)
+                return data.get("model")
+        except FileNotFoundError as error:
+            raise FileNotFoundError(
+                f"Model file not found at {self.model_path_IO}"
+            ) from error
+        except pickle.UnpicklingError as error:
+            raise pickle.UnpicklingError(f"Error loading model: {error}") from error
+        except Exception as error:
+            raise RuntimeError(
+                f"Unexpected error when loading model: {error}"
             ) from error
 
     def load_metadata(self) -> dict:
@@ -73,37 +144,12 @@ class ModelManager:
                 f"Unexpected error when loading metadata: {error}"
             ) from error
 
-    def save_model(self, model: BaseEstimator):
+    def check_if_loaded_properly(self, sample: np.ndarray, model: BaseEstimator = None):
         try:
-            pickl = {"model": model}
-            with open(self.model_path, "wb") as file:
-                pickle.dump(pickl, file)
-        except IOError as error:
-            raise IOError(f"Error saving model: {error}") from error
-        except Exception as error:
-            raise RuntimeError(
-                f"Unexpected error when saving model: {error}"
-            ) from error
-
-    def load_model(self) -> BaseEstimator:
-        try:
-            with open(self.model_path, "rb") as pickled:
-                data = pickle.load(pickled)
-                return data.get("model")
-        except FileNotFoundError as error:
-            raise FileNotFoundError(
-                f"Model file not found at {self.model_path}"
-            ) from error
-        except pickle.UnpicklingError as error:
-            raise pickle.UnpicklingError(f"Error loading model: {error}") from error
-        except Exception as error:
-            raise RuntimeError(
-                f"Unexpected error when loading model: {error}"
-            ) from error
-
-    def check_if_loaded_properly(self, sample: np.ndarray):
-        try:
-            model = self.load_model()
+            if model is None:
+                model = self.model
+                if model is None:
+                    raise ValueError("No model provided and no model available.")
 
             if model and hasattr(model, "predict"):
                 random_sample = sample[random.randint(0, sample.shape[0] - 1)]
@@ -117,3 +163,35 @@ class ModelManager:
             raise RuntimeError(
                 f"Error during model prediction check: {error}"
             ) from error
+
+    def _create_metadata_path(self, model_path: str) -> str:
+        """
+        Creates a metadata file path for the given model path by replacing
+        the model file extension with '.metadata.json'.
+
+        Parameters:
+            model_path (str): The path to the model file.
+
+        Returns:
+            str: The path to the metadata file.
+        """
+        if not model_path:
+            raise ValueError("Model path must be provided.")
+
+        # Split the model_path into the directory, base filename, and extension
+        directory, filename = os.path.split(model_path)
+        base_filename, _ = os.path.splitext(filename)
+
+        # Construct the metadata filename and its full path
+        metadata_filename = f"{base_filename}.metadata.json"
+        metadata_file_path = os.path.join(directory, metadata_filename)
+
+        return metadata_file_path
+
+    def _model_dir_exists(self, model_path_IO: str):
+        """
+        Checks if the directory for the model file exists.
+        """
+        model_dir = os.path.dirname(model_path_IO)
+        model_dir_exists = os.path.isdir(model_dir)
+        return model_dir, model_dir_exists
