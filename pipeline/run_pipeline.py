9433031d20074aa51fef137b4ec61025fa7f1b26
Karol Lukaszczyk <lukkarcontact@gmail.com>
2024-02-05 12:26:24 +0100

    Refactor pipeline script and add logging

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -1,13 +1,22 @@
 # Standard imports
 from dotenv import load_dotenv
 from pathlib import Path
+from subprocess import run, CalledProcessError
+from typing import Optional
+from datetime import datetime
+import logging
 import os
 import re
-import subprocess
 import sys
 
 
-def set_sys_path_to_project_root(__file__):
+def set_sys_path_to_project_root(__file__: str):
+    """
+    Adds the project root to the system path.
+
+    Args:
+        __file__ (str): The path to the current file.
+    """
     root_dir = Path(__file__).resolve().parent
     sys.path.append(str(root_dir))
 
@@ -17,16 +26,20 @@ set_sys_path_to_project_root(__file__)
 
 def initialize_environment_settings():
     """
-    Creates or verifies an .env file with predefined (dummy) environment variables for project configuration.
+    Creates or verifies an .env file
+    with predefined (dummy) environment variables
+    for project configuration.
 
     Raises:
         ValueError: If the .env file is not found or is empty.
     """
 
-    env_path = ".env"  # Assuming the root of the project is the project directory
+    env_path = Path(".env")  # Assuming the root of the project is the project directory
+    encoding = "utf-8"
 
     env_content = (
-        "USER_OFFERS_PATH=data\test\your_offers.csv\n"  # A structure example
+        # A structure example
+        f"USER_OFFERS_PATH={os.path.join('data', 'test', 'your_offers.csv')}\n"
         "MARKET_OFFERS_TIMEPLACE=\n"  # To be created after the scraping stage
         "MODEL_PATH=model.pkl\n"  # To be created after model training
         "LOCATION_QUERY=Warszawa\n"  # Be sure that location fits the query
@@ -37,10 +50,10 @@ def initialize_environment_settings():
     )
 
     if not env_path.exists():
-        with open(env_path, "w") as f:
+        with open(env_path, "w", encoding=encoding) as f:
             f.write(env_content)
 
-    if env_path.read_text() == env_content:
+    if env_path.read_text(encoding=encoding) == env_content:
         raise ValueError(
             "Please fill in the .env file with your data.\n"
             f"The file {env_path} is located at the root of the project.\n"
@@ -50,8 +63,14 @@ def initialize_environment_settings():
 
 initialize_environment_settings()
 
+logging.basicConfig(
+    filename=os.path.join("logs", "pipeline.log"),
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s:%(message)s",
+)
 
-def run_command(command, env_vars=None):
+
+def run_command(command: list[str], env_vars: Optional[dict] = None) -> int:
     """
     Run a command as a subprocess with optional environment variables.
 
@@ -59,37 +78,39 @@ def run_command(command, env_vars=None):
         command (list): The command to run as a list of strings.
         env_vars (dict, optional): Additional environment variables to set.
 
-    Returns:
-        int: The return code of the subprocess.
+    Raises:
+        CalledProcessError: If the command fails.
     """
     env = os.environ.copy()
     if env_vars:
         env.update(env_vars)
-    result = subprocess.run(command, env=env)
-    return result.returncode
+    try:
+        run(command, env=env, check=True)
+    except CalledProcessError as _error:
+        raise _error
 
 
-def run_python(script, env_vars=None):
+def run_python(script: list[str], env_vars: Optional[dict] = None):
     """
     Run a Python script as a subprocess.
     """
-    return run_command(["python", script], env_vars)
+    run_command(["python", script], env_vars)
 
 
-def run_ipynb(script, env_vars=None):
+def run_ipynb(script: list[str], env_vars: Optional[dict] = None):
     """
     Run a Jupyter notebook as a subprocess by converting it to a script first.
     """
-    return run_command(
+    run_command(
         ["jupyter", "nbconvert", "--to", "script", script, "--execute"], env_vars
     )
 
 
-def run_streamlit(script, env_vars=None):
+def run_streamlit(script: list[str], env_vars: Optional[dict] = None):
     """
     Run a Streamlit app as a subprocess.
     """
-    return run_command(["streamlit", "run", script], env_vars)
+    run_command(["streamlit", "run", script], env_vars)
 
 
 class PipelineError(Exception):
@@ -100,27 +121,54 @@ class PipelineError(Exception):
         super().__init__(self.message)
 
 
-def run_stage(stage):
+def run_stage(_stage: str):
     """
     Run a stage of the pipeline.
 
     Args:
-        stage (str): The path to the stage to run.
-
-    Returns:
-        int: The return code of the stage.
+        _stage (str): The path to the stage to run.
     """
-    if not os.path.exists(stage):
-        raise PipelineError(f"Stage not found: {stage}")
+    if not os.path.exists(_stage):
+        raise PipelineError(f"Stage not found: {_stage}")
 
-    if stage.endswith(".py") or re.match(r".*\.([a-z]|\_)+$", stage):  # TODO check
-        return run_python(stage)
-    elif stage.endswith(".ipynb"):
-        return run_ipynb(stage)
-    elif stage.endswith(".py"):
-        return run_streamlit(stage)
+    # https://regex101.com/r/JFd40X/1
+    pattern = r"^(?!.*\/[^\/]*\.[^\/]*$).*\/([^\/]+)$"
+    match_dir_path_only = re.match(pattern, _stage)
+
+    if _stage.endswith(".py") or match_dir_path_only:
+        run_python(_stage)
+    elif _stage.endswith(".ipynb"):
+        run_ipynb(_stage)
+    elif _stage.endswith(".py"):
+        run_streamlit(_stage)
     else:
-        raise ValueError(f"Unsupported file type: {stage}")
+        raise ValueError(f"Unsupported file type: {_stage}")
+
+
+def log_and_print(message: str, logging_level: int = logging.INFO):
+    """
+    Logs a message at the specified logging level
+    and prints it to the console with the current time.
+
+    Args:
+        message (str): The message to log and print.
+        logging_level (int, optional): The logging level.
+
+    Raises:
+        ValueError: If the logging level is not supported.
+    """
+
+    if logging_level == logging.INFO:
+        logging.info(message)
+    elif logging_level == logging.WARNING:
+        logging.warning(message)
+    elif logging_level == logging.ERROR:
+        logging.error(message)
+    else:
+        raise ValueError(f"Unsupported logging level: {logging_level}")
+
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{current_time}: {message}")
 
 
 if __name__ == "__main__":
@@ -146,8 +194,18 @@ if __name__ == "__main__":
     stages = scraping + cleaning_sub_stageg + model_developing + data_visualizing
 
     for stage in stages:
-        print(f"Running {stage}...")
-        return_code = run_stage(stage)
-        if return_code != 0:
-            raise PipelineError(f"Error in {stage}. Exiting pipeline.")
-    print("Pipeline execution completed.")
+        log_and_print(f"Running {stage}...")
+
+        try:
+            # Attempt to run the stage, which may raise CalledProcessError on failure
+            run_stage(stage)
+            log_and_print(f"{stage} completed successfully.")
+        except CalledProcessError as error:
+            # Log the error and terminate the pipeline on failure
+            error_message = (
+                f"Error in {stage} with exit code {error.returncode}. Exiting pipeline."
+            )
+            log_and_print(error_message, logging.ERROR)
+            raise PipelineError(error_message) from error
+
+    log_and_print("Pipeline execution completed.")
